d7b2a1e2fe01392c9b55d1db5a9b43f026091f4f
Joshua Dahl <joshuadahl@nevada.unr.edu>
2021-10-08 14:40:52 -0700

    Mipmaps
    Textures now automatically generate mipmaps when they are imported (sampled using tri-linear filtering).

--- PA7/src/object.cpp
+++ PA7/src/object.cpp
@@ -164,8 +164,9 @@ bool Object::LoadTextureFile(const Arguments& args, std::string path, bool makeR
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	// Free the image
 	stbi_image_free(img);
