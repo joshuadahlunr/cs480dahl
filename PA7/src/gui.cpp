b12f99ab39a2b3cc1a35f6e568ba176ac3e7ece1
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-10-11 02:02:13 -0700

    Added key controls to menu bar
    Now the user can click the "Help" button to see all the possible key bindings.

--- PA7/src/gui.cpp
+++ PA7/src/gui.cpp
@@ -48,6 +48,17 @@ GUI::ShouldProcessEvents GUI::ProcessEvent(SDL_Event& event){
 	return { !io.WantCaptureKeyboard, !io.WantCaptureMouse };
 }
 
+void TextCenter(std::string text) {
+    float font_size = ImGui::GetFontSize() * text.size() / 2;
+    ImGui::SameLine(
+        ImGui::GetWindowSize().x / 2 -
+        font_size + (font_size / 2)
+    );
+
+    ImGui::Text(text.c_str());
+	ImGui::NewLine();
+}
+
 void GUI::Render(){
 	// Start the Dear ImGui frame
 	ImGui_ImplOpenGL3_NewFrame();
@@ -63,6 +74,77 @@ void GUI::Render(){
 
 			if (ImGui::MenuItem((globalShouldScale ? "Use Actual Data" : "Use Scaled Data")))
 				globalShouldScale = !globalShouldScale;
+
+			ImGui::EndMenu();
+		}
+
+		if(ImGui::BeginMenu("Help")) {
+			ImGui::NewLine();
+			ImGui::NewLine();
+			TextCenter("Solar System");
+			ImGui::NewLine();
+			TextCenter("A 2021 CS 480/680 Group Project");
+			TextCenter("- Joshua Dahl");
+			TextCenter("- Bryan Dedeurwaerder");
+			TextCenter("- Jonathan Peters");
+			ImGui::NewLine();
+			TextCenter("University of Nevada, Reno");
+			ImGui::NewLine();
+
+
+
+			ImGui::TextColored(ImVec4(.9,.9,1,1), "Key Bindings");
+			if (ImGui::BeginTable("", 3, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg)) {
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::TableHeader("Device");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::TableHeader("Key");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::TableHeader("Action");
+
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::Text("Mouse");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::Text("Right Button");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::Text("Move Camera");
+				
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::Text(" ");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::Text("Scroll Wheel");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::Text("Zoom");
+				
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::Text("KeyBoard");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::Text("Tab");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::Text("Change Focus Celestial");
+
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::Text(" ");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::Text("Left Shift");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::Text("Slow Zoom Speed");				
+					
+				ImGui::TableNextRow();
+					ImGui::TableSetColumnIndex(0);
+					ImGui::Text(" ");
+					ImGui::TableSetColumnIndex(1);
+					ImGui::Text("Escape");
+					ImGui::TableSetColumnIndex(2);
+					ImGui::Text("Quit");
+			
+				ImGui::EndTable();
+			}
 
 			ImGui::EndMenu();
 		}
