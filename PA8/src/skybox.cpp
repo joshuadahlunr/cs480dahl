8ccf8416c3f12ad4c714cf0644ccdf0a30c1c229
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2021-10-24 11:17:28 -0700

    Added cloudy sky skybox
    Cloudy sky makes objects a little more visible. The skybox folder is now extended with multiple skyboxs.

--- PA8/src/skybox.cpp
+++ PA8/src/skybox.cpp
@@ -95,7 +95,7 @@ bool Skybox::Initialize(const Arguments& args, Camera* camera){
 	}
 
 	// Load the cube map
-	success &= LoadCubeTexture(args, args.getResourcePath() + "textures/skybox");
+	success &= LoadCubeTexture(args, args.getResourcePath() + "textures/skyboxs/clouds");
 
 	return success;
 }
