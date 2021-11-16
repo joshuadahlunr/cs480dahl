fe1c570ed63c57f371fde7587d8d62214403174f
Joshua Dahl <joshuadahl@nevada.unr.edu>
2021-11-15 22:50:29 -0800

    Added Functions to Convert Between React and GLM
    Added some conversion functions to physics.h which allow for easier interop between react and glm.

--- PA11/include/physics.h
+++ PA11/include/physics.h
@@ -64,4 +64,50 @@ protected:
 #endif
 };
 
+
+// -- React Interop --
+
+
+// Convert vector3s
+static rp3d::Vector3 toReact(const glm::vec3& v) { return rp3d::Vector3(v.x, v.y, v.z); }
+static rp3d::Vector3 toReact(const glm::vec3&& v) { return toReact(v); }
+// Convert quaternions
+static rp3d::Quaternion toReact(const glm::quat& q) {
+	auto rotationMat = glm::mat3_cast(q);
+	float* rot = glm::value_ptr(rotationMat);
+	return { rp3d::Matrix3x3(rot[0], rot[1], rot[2], rot[3], rot[4], rot[5], rot[6], rot[7], rot[8]) };
+}
+static rp3d::Quaternion toReact(const glm::quat&& q) { return toReact(q); }
+// Convert vec3 and quaternion to transform
+static rp3d::Transform toReact(const glm::vec3 pos, const glm::quat rotation){
+	rp3d::Transform out;
+	out.setPosition(toReact(pos));
+	out.setOrientation(toReact(rotation));
+	return out;
+}
+// Convert mat4 to transform
+static rp3d::Transform toReact(const glm::mat4& mat){
+	rp3d::Transform out;
+	out.setFromOpenGL((rp3d::decimal*) glm::value_ptr(mat));
+	return out;
+}
+static rp3d::Transform toReact(const glm::mat4&& mat){ return toReact(mat); }
+
+// Convert vector3s
+static glm::vec3 toGLM(const rp3d::Vector3& v) { return {v.x, v.y, v.z}; }
+static glm::vec3 toGLM(const rp3d::Vector3&& v) { return toGLM(v); }
+// Convert quaternions
+static glm::quat toGLM(const rp3d::Quaternion& q){
+	auto mat = q.getMatrix();
+	return glm::quat_cast(glm::mat3(mat[0][0], mat[0][1], mat[0][2], mat[1][0], mat[1][1], mat[1][2], mat[2][0], mat[2][1], mat[2][2]));
+}
+static glm::quat toGLM(const rp3d::Quaternion&& q){ return toGLM(q); }
+// Convert transform to mat4
+static glm::mat4 toGLM(const rp3d::Transform& t){
+	glm::mat4 out;
+	t.getOpenGLMatrix(glm::value_ptr(out));
+	return out;
+}
+static glm::mat4 toGLM(const rp3d::Transform&& t){ return toGLM(t); }
+
 #endif /* PHYSICS_H */
