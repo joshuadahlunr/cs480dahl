451122f56df5f6938f1bf80ab3e47ca4448cc016
Joshua Dahl <joshuadahl@nevada.unr.edu>
2021-12-06 14:05:21 -0800

    NPC Patch
    Fixed NPCs timer so it supports more than one waypoint.

--- PA11/src/npc.cpp
+++ PA11/src/npc.cpp
@@ -5,28 +5,32 @@ NPC::NPC() {}
 NPC::~NPC() {}
 
 void NPC::update(float dt) {
-    // Add waypoints if none
-    if (waypoints.size() < 1) {
-        float x = rand() % (int) (wanderDistance * 2 + 1) - (wanderDistance) + getPosition().x;
-        float z = rand() % (int) (wanderDistance * 2 + 1) - (wanderDistance) + getPosition().z;
-        waypoints.push(glm::vec3(x, 0, z));
-        waitTimes.push(rand() % (5));
-        currentWait = waitTimes.front();
-    }
+	// Add waypoints if none
+	if (waypoints.size() < 1) {
+		float x = rand() % (int) (wanderDistance * 2 + 1) - (wanderDistance) + getPosition().x;
+		float z = rand() % (int) (wanderDistance * 2 + 1) - (wanderDistance) + getPosition().z;
+		waypoints.push(glm::vec3(x, 0, z));
+		waitTimes.push(rand() % (5));
+	}
 
-    // decrement a wait
-    if (glm::distance(waypoints.front(), getPosition()) < 1.0f) {
-        currentWait = currentWait - dt; 
-    } else {
-        glm::vec3 direction = glm::normalize(waypoints.front() - getPosition());
-        
-        float speed = 5;
-        setPosition(getPosition() + (direction * speed * dt));
-    }
+	if(waypoints.empty()) return;
 
-    if (currentWait < 0) {
-        waypoints.pop();
-    } 
+	// decrement a wait
+	if (glm::distance(waypoints.front(), getPosition()) < 1.0f) {
+		currentWait -= dt;
+
+		if (currentWait < 0) {
+			waypoints.pop();
+			currentWait = waitTimes.front();
+		}
+	} else {
+		glm::vec3 direction = glm::normalize(waypoints.front() - getPosition());
+
+		float speed = 5;
+		setPosition(getPosition() + (direction * speed * dt));
+	}
+
+
 
 
 }
